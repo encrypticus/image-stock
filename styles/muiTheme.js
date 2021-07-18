d6c2dd2741b28a4ff7825dc914d37feab0cd0698
encrypticus <encrypticus@gmail.com>
2021-07-18 23:52:40 +0600

    refactor: styles/theme.js -> styles/muiTheme.js

--- styles/muiTheme.js
+++ styles/muiTheme.js
@@ -1,7 +1,7 @@
 import { createMuiTheme } from '@material-ui/core';
 import { green } from '@material-ui/core/colors';
 
-export const theme = createMuiTheme({
+export const muiTheme = createMuiTheme({
   overrides: {
     MuiMenu: {
       list: {
@@ -20,7 +20,7 @@ export const theme = createMuiTheme({
         color: green[400],
         '&$checked': {
           color: `${green[600]}!important`,
-        }
+        },
       },
     },
 
