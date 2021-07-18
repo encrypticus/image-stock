864d7b55c1c3456747767fb7ee37ed1f4c305b87
encrypticus <encrypticus@gmail.com>
2021-07-18 19:32:16 +0600

    Добавил библиотеку framer-motion и анимацию появления новых изображений

--- components/ImageList/ImageListItem/ImageListItem.jsx
+++ components/ImageList/ImageListItem/ImageListItem.jsx
@@ -1,4 +1,5 @@
 import React, { memo } from 'react';
+import { motion } from 'framer-motion';
 
 import { ListItem } from '@material-ui/core';
 import PropTypes from 'prop-types';
@@ -7,19 +8,22 @@ import { useStyles } from './styles';
 
 export const ImageListItem = memo((props) => {
   const styles = useStyles();
-  const {
-    src, tags,
-    width, height
-  } = props;
-  const processedSrc = src.replace(/_(\d+)\.(.+)$/, (_, num, ext) => '__'+340+'.'+ext );
+  const { src, tags, width, height } = props;
+  const processedSrc = src.replace(/_(\d+)\.(.+)$/, (_, num, ext) => '__' + 340 + '.' + ext);
+  const variants = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  };
 
   return (
-    <ListItem className={`${styles.root} grid-item`}>
-      <img
-        className={styles.img}
-        src={processedSrc}
-        alt={tags}
-      />
+    <ListItem
+      className={`${styles.root} grid-item`}
+      component={motion.li}
+      initial={'hidden'}
+      animate={'visible'}
+      variants={variants}
+    >
+      <img className={styles.img} src={processedSrc} alt={tags} />
     </ListItem>
   );
 });
