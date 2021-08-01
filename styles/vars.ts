949a139e55fec73d9d5a43698a453bd0b278acae
encrypticus <encrypticus@gmail.com>
2021-08-01 22:39:09 +0600

    Добавил файл темы для styled components

---
+++ styles/vars.ts
@@ -0,0 +1,8 @@
+import { mobileWidth, tabletWidth } from 'utils';
+
+export const vars = {
+  media: {
+    mobile: `(max-width: ${mobileWidth}px)`,
+    tablet: `(max-width: ${tabletWidth}px)`,
+  },
+};
